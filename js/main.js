d0824c3ac541921e88c6f789dc78b43561d4094d
CQD <wesleyliao@kkbox.com>
2014-02-15 17:40:16 +0800

    加上 Mozilla 風格的下拉功能選單

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(function(){
+	var $mainMenu = $('#nav .menu');
+	$('#nav .menuToggle').on('click touchstart', function(){
+		if($mainMenu.is(':hidden')){
+			$mainMenu.slideDown(230);
+		} else {
+			$mainMenu.slideUp(230);
+		}
 
+	});
+});
